4552506406d901381e6e81b93539333a55a55382
JuliandsonBarros <juliandsonbarros@gmail.com>
2023-01-03 15:24:45 -0300

    metodos de busca e pesquisa de Usuario/Service

--- src/main/java/br/com/controlefinanceiro/seviceImpl/UsuarioServiceImpl.java
+++ src/main/java/br/com/controlefinanceiro/seviceImpl/UsuarioServiceImpl.java
@@ -1,5 +1,21 @@
 package br.com.controlefinanceiro.seviceImpl;
 
-public class UsuarioServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+import br.com.controlefinanceiro.domain.Usuario;
+import br.com.controlefinanceiro.repository.UsuarioRepository;
+
+public class UsuarioServiceImpl {
+	
+	private UsuarioRepository repo;
+	
+	public List<Usuario> findAll(){
+		return repo.findAll();
+	}
+
+	public Usuario findId(Integer id) {
+		Optional<Usuario> usuario = repo.findById(id);
+		return usuario.orElse(null);
+	}
 }
